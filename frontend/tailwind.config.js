e3d34c31b91841acbf8751e9731140f9ee70665f
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-09-11 23:15:55 +0300

    tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode : false,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
     extend: {
-      backgroundColor : {
-        'dark-bg' : '#001529',
-        'light-bg' : '#f3f4f6'
-      }
+      backgroundColor: {
+        "dark-bg": "#075e54",
+        "light-bg": "#ece5dd",
+      },
     },
   },
   plugins: [],
-}
+};
